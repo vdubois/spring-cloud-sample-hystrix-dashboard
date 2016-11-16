192143025a1d28c35f2363c0487614265aabcdd0
Vincent Dubois <dubois.vct@free.fr>
2016-11-16 21:24:25 +0100

    No need to register Hystrix dashboard with Eureka

--- src/main/java/io/github/vdubois/SpringCloudSampleHystrixDashboardApplication.java
+++ src/main/java/io/github/vdubois/SpringCloudSampleHystrixDashboardApplication.java
@@ -2,12 +2,10 @@ package io.github.vdubois;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableHystrixDashboard
-@EnableDiscoveryClient
 public class SpringCloudSampleHystrixDashboardApplication {
 
 	public static void main(String[] args) {
